f3be720acfc7aa09a1d2d4ed51e5a555455fdb91
Dabuaf86 <dabuaf@gmail.com>
2022-01-02 21:57:55 -0300

    last modifications to test deployment. Frontend proxy added to pckg.json

--- api/src/app.js
+++ api/src/app.js
@@ -1,36 +1,61 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const morgan = require("morgan");
-const routes = require("./routes/index.js");
-
-require("./db.js");
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const routes = require('./routes/index.js');
+// const cors = require('cors');
+require('./db.js');
 
 const server = express();
 
-server.name = "API";
+server.name = 'API';
 
-server.use(express.urlencoded({ extended: true, limit: "50mb" }));
-server.use(express.json({ limit: "50mb" }));
+server.use(express.urlencoded({ extended: true, limit: '50mb' }));
+server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
-server.use(morgan("dev"));
+server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
+	res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+	res.header('Access-Control-Allow-Credentials', 'true');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	);
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+	next();
 });
+
+// Middleware added for Heroku deployment
+// const whiteList = ['http://localhost:3001', 'http://localhost:3000'];
+// const corsOptions = {
+// 	origin: (origin, callback) => {
+// 		console.log('** Origin of request ' + origin);
+// 		if (whiteList.indexOf(origin) !== -1 || !origin) {
+// 			console.log('Origin acceptable');
+// 			callback(null, true);
+// 		} else {
+// 			console.log('Origin rejected');
+// 			callback(new Error('Not allowed by CORS'));
+// 		}
+// 	},
+// };
+
+// server.use(cors(corsOptions));
+
+// Also added for deployment
+if (process.env.NODE_ENV === 'production') {
+	server.use(express.static(this.path.join(__dirname, 'client/build')));
+	server.get('*', (req, res) =>
+		res.sendFile(this.path.join(__dirname, 'client/build', 'index.html'))
+	);
+}
 
 // Error catching endware.
 server.use((err, req, res, next) => {
-  // eslint-disable-line no-unused-vars
-  const status = err.status || 500;
-  const message = err.message || err;
-  console.error(err);
-  res.status(status).send(message);
+	// eslint-disable-line no-unused-vars
+	const status = err.status || 500;
+	const message = err.message || err;
+	console.error(err);
+	res.status(status).send(message);
 });
 
 server.use(routes);
