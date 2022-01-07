de70e2d89a14c6516d92fee8f5f4fe2964630772
Dabuaf86 <dabuaf@gmail.com>
2022-01-07 16:00:48 -0300

    api subdir ready for deployment

--- api/index.js
+++ api/index.js
@@ -6,8 +6,8 @@ const { GENRE_URL, PLATFORM_URL } = require('./src/utils/urls');
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-	server.listen(process.env.PORT || 3001, () => {
-		console.log('%s listening at 3001'); // eslint-disable-line no-console
+	server.listen(process.env.PORT, () => {
+		console.log(`%s listening at ${process.env.PORT}`); // eslint-disable-line no-console
 		axios
 			.get(`${GENRE_URL}?key=${API_KEY}`)
 			.then(
