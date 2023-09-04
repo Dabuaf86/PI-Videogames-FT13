d85e48f33c25017b7ae6533766cc95c661df409c
Dabuaf86 <dabuaf@gmail.com>
2023-09-04 12:04:21 -0300

    db persistent

--- api/index.js
+++ api/index.js
@@ -1,34 +1,34 @@
-const server = require("./app.js");
-const { conn, Genre, Platform } = require("./src/db.js");
-const axios = require("axios");
+const server = require('./app.js');
+const { conn, Genre, Platform } = require('./src/db.js');
+const axios = require('axios');
 const { API_KEY } = process.env;
-const { LOCAL_PORT, GENRE_URL, PLATFORM_URL } = require("./src/utils/urls");
+const { LOCAL_PORT, GENRE_URL, PLATFORM_URL } = require('./src/utils/urls');
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
-  server.listen(LOCAL_PORT, () => {
-    console.log(`Servidor escuchando en puerto ${LOCAL_PORT}`); // eslint-disable-line no-console
-    axios
-      .get(`${GENRE_URL}?key=${API_KEY}`)
-      .then(
-        (genres) =>
-          genres.data.results &&
-          genres.data.results.forEach((result) =>
-            Genre.create({ name: result.name }).catch((error) =>
-              console.error(error)
-            )
-          )
-      );
-    axios
-      .get(`${PLATFORM_URL}?key=${API_KEY}`)
-      .then(
-        (platforms) =>
-          platforms.data.results &&
-          platforms.data.results.forEach((result) =>
-            Platform.create({ name: result.name }).catch((error) =>
-              console.error(error)
-            )
-          )
-      );
-  });
+conn.sync(/*{ force: true }*/).then(() => {
+	server.listen(LOCAL_PORT, () => {
+		console.log(`Servidor escuchando en puerto ${LOCAL_PORT}`); // eslint-disable-line no-console
+		axios
+			.get(`${GENRE_URL}?key=${API_KEY}`)
+			.then(
+				genres =>
+					genres.data.results &&
+					genres.data.results.forEach(result =>
+						Genre.create({ name: result.name }).catch(error =>
+							console.error(error)
+						)
+					)
+			);
+		axios
+			.get(`${PLATFORM_URL}?key=${API_KEY}`)
+			.then(
+				platforms =>
+					platforms.data.results &&
+					platforms.data.results.forEach(result =>
+						Platform.create({ name: result.name }).catch(error =>
+							console.error(error)
+						)
+					)
+			);
+	});
 });
